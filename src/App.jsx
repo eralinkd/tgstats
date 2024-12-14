837bbdc003262632cbd99ea87bde7c2974d2912b
eralinkd <eralinkd@gmail.com>
2024-12-14 17:27:22 +0200

    initial (maybe)

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,18 @@
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+
+import Header from './shared/Header/Header';
+import Home from './views/Home/Home';
+import Profile from './views/Profile/Profile';
+
 function App() {
   return (
-    <>
-      <div className="">hello worlddsds</div>
-    </>
+    <BrowserRouter>
+        <Header />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/profile" element={<Profile />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
